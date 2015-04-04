ba8135a1e15206abe3ca595570d03099f86f5b94
Bill Cao <billcao@berkeley.edu>
2015-04-03 22:26:37 -0700

    Fourth pass of cleanup non-java source files removed

--- src/main/python/lib/fetch_protein.py
+++
@@ -1,9 +0,0 @@
-from ClothoPy.protein_retrieval import call_accn
-
-def _fetch_protein(polypeptide):
-	return polypeptide.toJSON()
-
-def run(accession_id):
-	retriever = call_accn('protein', 'gb', 'nobody@example.com')
-    retriever.retrieve_gb(accession_ids)
-    return _fetch_protein(retriever.records[0]) #map(_fetch_protein, retriever.records)
--- src/main/python/lib/hello.py
+++
@@ -1,2 +0,0 @@
-def hi():
-    return "greetings user"
